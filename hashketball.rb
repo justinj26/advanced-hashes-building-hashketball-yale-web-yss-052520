269c9a4d8dc8e20ec9e3f11315997ddd4bd7120e
Justin James <justin.james@yale.edu>
2020-05-10 01:33:47 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -90,7 +90,7 @@ def game_hash
         blocks: 5,
         slam_dunks: 5 
       }, {
-        player_name: "Ben Gordon"
+        player_name: "Ben Gordon",
         number: 8,
         shoe: 15,
         points: 33,
@@ -100,7 +100,7 @@ def game_hash
         blocks: 1, 
         slam_dunks: 0
       }, {
-        player_name: "Kemba Walker"
+        player_name: "Kemba Walker",
         number: 33,
         shoe: 15, 
         points: 6,
