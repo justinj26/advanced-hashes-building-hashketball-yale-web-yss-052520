585e3b954bcc85f12161fe657cd1d5e573ac15c5
Justin James <justin.james@yale.edu>
2020-05-10 01:04:26 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -9,7 +9,7 @@ def game_hash
     :away => {
       :team_name => "Charlotte Hornets",
       :colors => ["Turqoise", "Purple"],
-      :players =>  []
+      :players =>  [{}, {}, {}, {}, {}]
     }
   }
 end
