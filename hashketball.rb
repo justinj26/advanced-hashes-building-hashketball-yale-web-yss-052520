9f5f835173d0587e45f5bf42aea46946da2bb147
Justin James <justin.james@yale.edu>
2020-05-10 01:19:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,53 +6,53 @@ def game_hash
       :colors => ["Black", "White"],
       :players => [{
         player_name: "Alan Anderson",
-        number:
-        shoe:
-        points:
-        rebounds:
-        assists:
-        steals:
-        blocks:
-        slam_dunks:
+        number: 0,
+        shoe: 16,
+        points: 22, 
+        rebounds: 12, 
+        assists: 12, 
+        steals: 3, 
+        blocks: 1,
+        slam_dunks: 1 
       }, {
-        player_name:
-        number:
-        shoe:
-        points:
-        rebounds:
-        assists:
-        steals:
-        blocks:
-        slam_dunks:
+        player_name: "Reggie Evans", 
+        number: 30, 
+        shoe: 14,
+        points: 12,
+        rebounds: 12, 
+        assists: 12,
+        steals: 12,
+        blocks: 12,
+        slam_dunks: 7
       }, {
-        player_name:
-        number:
-        shoe:
-        points:
-        rebounds:
-        assists:
-        steals:
-        blocks:
-        slam_dunks:
+        player_name: "Brook Lopez",
+        number: 11,
+        shoe: 17, 
+        points: 17,
+        rebounds: 19,
+        assists: 10, 
+        steals: 3, 
+        blocks: 1, 
+        slam_dunks: 15
       }, {
-        player_name:
-        number:
-        shoe:
-        points:
-        rebounds:
-        assists:
-        steals:
-        blocks:
-        slam_dunks:
+        player_name: "Mason Plumlee",
+        number: 1,
+        shoe: 19,
+        points: 26,
+        rebounds: 11,
+        assists: 6,
+        steals: 3,
+        blocks: 8, 
+        slam_dunks: 5 
       }, {
-        player_name:
-        number:
-        shoe:
-        points:
-        rebounds:
-        assists:
-        steals:
-        blocks:
+        player_name: "Jason Terry",
+        number: ,
+        shoe: 19,
+        points: 2, 
+        rebounds: 2,
+        assists: 4, 
+        steals: 11,
+        blocks: 
         slam_dunks:
       }]
     },
@@ -61,14 +61,14 @@ def game_hash
       :colors => ["Turqoise", "Purple"],
       :players =>  [{
         player_name: "Jeff Adrien",
-        number:
-        shoe:
-        points:
-        rebounds:
-        assists:
-        steals:
-        blocks:
-        slam_dunks:
+        number: 4,
+        shoe: 18,
+        points: 10, 
+        rebounds: 1, 
+        assists: 1, 
+        steals: 2, 
+        blocks: 7,
+        slam_dunks: 2 
       }, {
         player_name:
         number:
